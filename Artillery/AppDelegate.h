8862b426be8074b25516963c97661b17661065ca
John Bender <john@generalui.com>
2012-05-03 15:29:03 -0700

    grading

--- Artillery/AppDelegate.h
+++ Artillery/AppDelegate.h
@@ -12,4 +12,14 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+/*
+ Good job, you completed all the requirements. Obviously there is room for
+ additional polish, which we're pretty big on at General UI. But I know you
+ prioritized your robot, and that's fine. Let me know when I can see it in
+ action!
+ 
+ Grade: 100%
+ Course grade: 99% (A)
+*/
+
 @end
